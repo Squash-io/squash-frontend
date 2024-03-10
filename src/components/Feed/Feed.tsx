899f80903e05aa5687a47214222b35a780c2a1e4
Soyoung Park <soyoung008@naver.com>
2024-03-11 01:13:01 +0900

    design : 예시 코드 삭제 및 Header 컴포넌트 불러옴

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,16 +1,4 @@
-import Txt from '../../constants/Txt/Txt';
-import { COLORS } from '../../constants/Color/Color';
-import { IcBell } from '../../assets';
+import Header from '../@common/Header';
 export const Feed = () => {
-  return (
-    <>
-      <Txt color={COLORS.baseColors.gray400} textStyleName="body1">
-        Home
-      </Txt>
-      <IcBell />
-      <Txt color={COLORS.primary.purple500} textStyleName="body1">
-        Home2
-      </Txt>
-    </>
-  );
+  return <Header />;
 };
