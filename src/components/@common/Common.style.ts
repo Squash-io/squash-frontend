c632f1ef4f7c177ee1aaabda73363ca4fe098f95
Soyoung Park <soyoung008@naver.com>
2024-03-11 00:51:34 +0900

    design : headerUI 구현

---
+++ src/components/@common/Common.style.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const HeaderContainer = styled.section`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding: 1.6rem;
+`;
---
+++ src/components/@common/Header.tsx
@@ -0,0 +1,13 @@
+import { IcBell, IcSquash } from '../../assets';
+import { HeaderContainer } from './Common.style';
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <IcSquash />
+      <IcBell />
+    </HeaderContainer>
+  );
+};
+
+export default Header;
