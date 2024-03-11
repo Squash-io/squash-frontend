5104e66443d71c8b663c6cf8c93ef0554f27f1b5
Soyoung Park <soyoung008@naver.com>
2024-03-12 01:35:49 +0900

    feat : 라우터 outlet 구현

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './components/@common/Layout/Layout';
 import Home from './pages/Home/Home';
+import Feed from './pages/Feed/Feed';
+import Friend from './pages/Friend/Friend';
+import My from './pages/My/My';
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="" element={<Home />} />
+        <Route path="" element={<Home />}>
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/friend" element={<Friend />} />
+          <Route path="/my" element={<My />} />
+        </Route>
       </Route>
     </Routes>
   );
