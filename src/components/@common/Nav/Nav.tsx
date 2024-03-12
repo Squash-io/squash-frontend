6a6e2357226659c90b44c32e6928882c939131fe
Soyoung Park <soyoung008@naver.com>
2024-03-12 18:55:25 +0900

    design : 클릭된 아이템 글자색 변경

--- src/components/@common/Nav/Nav.tsx
+++ src/components/@common/Nav/Nav.tsx
@@ -7,12 +7,14 @@ import { useState } from 'react';
 const Nav = () => {
   const navigate = useNavigate();
   const [navItems, setNavItems] = useState([
-    { id: 'feed', text: '피드' },
-    { id: 'friend', text: '친구' },
-    { id: 'my', text: '마이' },
+    { id: 'feed', text: '피드', selected: true },
+    { id: 'friend', text: '친구', selected: false },
+    { id: 'my', text: '마이', selected: false },
   ]);
 
   function handleNavItemClicked(itemId: string) {
+    const updatedItems = navItems.map((item) => ({ ...item, selected: item.id === itemId }));
+    setNavItems(updatedItems);
     navigate(`/${itemId}`);
   }
 
@@ -20,7 +22,10 @@ const Nav = () => {
     <NavContainer>
       {navItems.map((item) => (
         <NavItem key={item.id} type="button" onClick={() => handleNavItemClicked(item.id)}>
-          <Txt color={COLORS.baseColors.gray400} textStyleName="body1">
+          <Txt
+            color={item.selected ? COLORS.baseColors.gray950 : COLORS.baseColors.gray400}
+            textStyleName="body1"
+          >
             {item.text}
           </Txt>
         </NavItem>
