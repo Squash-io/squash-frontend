3fa1c9d46d9d6bd33a97c119e64f01c196d7d8e2
Soyoung Park <soyoung008@naver.com>
2024-03-12 18:01:52 +0900

    feat : 네비게이션바 클릭시 해당 페이지로 이동

--- src/components/@common/Nav/Nav.tsx
+++ src/components/@common/Nav/Nav.tsx
@@ -1,21 +1,32 @@
+import { useNavigate } from 'react-router-dom';
 import { COLORS } from '../../../constants/Color/Color';
 import Txt from '../../../constants/Txt/Txt';
 import { NavContainer, NavItem } from './Nav.style';
 
 const Nav = () => {
+  const navigate = useNavigate();
+  function handleMoveToFeed() {
+    navigate('/feed');
+  }
+  function handleMoveToFriend() {
+    navigate('/friend');
+  }
+  function handleMoveToMy() {
+    navigate('/my');
+  }
   return (
     <NavContainer>
-      <NavItem type="button">
+      <NavItem type="button" onClick={handleMoveToFeed}>
         <Txt color={COLORS.baseColors.gray400} textStyleName="body1">
           피드
         </Txt>
       </NavItem>
-      <NavItem type="button">
+      <NavItem type="button" onClick={handleMoveToFriend}>
         <Txt color={COLORS.baseColors.gray400} textStyleName="body1">
           친구
         </Txt>
       </NavItem>
-      <NavItem type="button">
+      <NavItem type="button" onClick={handleMoveToMy}>
         <Txt color={COLORS.baseColors.gray400} textStyleName="body1">
           마이
         </Txt>
