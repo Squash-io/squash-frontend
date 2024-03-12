41d70d76c4873216ee9f9dafc643766e9d5b5a59
Soyoung Park <soyoung008@naver.com>
2024-03-12 18:44:47 +0900

    feat : map 이용해서 코드 변경

--- src/components/@common/Nav/Nav.tsx
+++ src/components/@common/Nav/Nav.tsx
@@ -2,35 +2,29 @@ import { useNavigate } from 'react-router-dom';
 import { COLORS } from '../../../constants/Color/Color';
 import Txt from '../../../constants/Txt/Txt';
 import { NavContainer, NavItem } from './Nav.style';
+import { useState } from 'react';
 
 const Nav = () => {
   const navigate = useNavigate();
-  function handleMoveToFeed() {
-    navigate('/feed');
+  const [navItems, setNavItems] = useState([
+    { id: 'feed', text: '피드' },
+    { id: 'friend', text: '친구' },
+    { id: 'my', text: '마이' },
+  ]);
+
+  function handleNavItemClicked(itemId: string) {
+    navigate(`/${itemId}`);
   }
-  function handleMoveToFriend() {
-    navigate('/friend');
-  }
-  function handleMoveToMy() {
-    navigate('/my');
-  }
+
   return (
     <NavContainer>
-      <NavItem type="button" onClick={handleMoveToFeed}>
-        <Txt color={COLORS.baseColors.gray400} textStyleName="body1">
-          피드
-        </Txt>
-      </NavItem>
-      <NavItem type="button" onClick={handleMoveToFriend}>
-        <Txt color={COLORS.baseColors.gray400} textStyleName="body1">
-          친구
-        </Txt>
-      </NavItem>
-      <NavItem type="button" onClick={handleMoveToMy}>
-        <Txt color={COLORS.baseColors.gray400} textStyleName="body1">
-          마이
-        </Txt>
-      </NavItem>
+      {navItems.map((item) => (
+        <NavItem key={item.id} type="button" onClick={() => handleNavItemClicked(item.id)}>
+          <Txt color={COLORS.baseColors.gray400} textStyleName="body1">
+            {item.text}
+          </Txt>
+        </NavItem>
+      ))}
     </NavContainer>
   );
 };
