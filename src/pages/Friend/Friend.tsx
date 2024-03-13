f8ab7d1601e83f6b00dc65c3f08c475fbc756737
Soyoung Park <102568726+qwp0@users.noreply.github.com>
2024-03-13 10:54:56 +0900

    feat : home 레이아웃 구현 (#7)

---
+++ src/pages/Friend/Friend.tsx
@@ -0,0 +1,5 @@
+const Friend = () => {
+  return <div>friend</div>;
+};
+
+export default Friend;
--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
-import { Feed } from './components/Feed/Feed';
 import { Layout } from './components/@common/Layout/Layout';
+import Home from './pages/Home/Home';
+import Feed from './pages/Feed/Feed';
+import Friend from './pages/Friend/Friend';
+import My from './pages/My/My';
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="" element={<Feed />} />
+        <Route path="" element={<Home />}>
+          <Route index element={<Feed />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/friend" element={<Friend />} />
+          <Route path="/my" element={<My />} />
+        </Route>
       </Route>
     </Routes>
   );
