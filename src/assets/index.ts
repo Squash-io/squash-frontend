df0b91751a6318c592d17c014e141810f38f5b95
Soyoung Park <soyoung008@naver.com>
2024-03-13 15:45:56 +0900

    assets : 배너에 필요한 icon 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,6 @@
 import IcBell from './icon/fi_bell.svg?react';
 import IcSquash from './icon/squash.svg?react';
+import IcGithub from './icon/fi_github.svg?react';
+import IcChevronRight from './icon/fi_chevron-right.svg?react';
 
-export { IcBell, IcSquash };
+export { IcBell, IcSquash, IcGithub, IcChevronRight };
