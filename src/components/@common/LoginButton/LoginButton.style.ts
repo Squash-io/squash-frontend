55afeb202a75d4925080f593d128f56b1f726107
Soyoung Park <soyoung008@naver.com>
2024-05-25 12:17:12 +0900

    feat : 로그인 버튼 공통 컴포넌트로 구현

---
+++ src/components/@common/LoginButton/LoginButton.style.ts
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+import { COLORS } from '../../../constants/Color/Color';
+
+export const LoginButtonContainer = styled.button`
+  display: flex;
+  padding: 0.9rem 7rem;
+  align-items: center;
+  background-color: ${COLORS.baseColors.gray950};
+  gap: 3.2rem;
+  border-radius: 1.2rem;
+  cursor: pointer;
+`;
